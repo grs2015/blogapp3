b1167de50ab795ba0ef8f21f40aff54366b8647d
grs2015 <85684748+grs2015@users.noreply.github.com>
2022-06-21 01:13:31 +0200

    Func: added columns in posts table, Post model edited, doctrine/dbal added

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    const PUBLISHED = 'published';
+    const DRAFT = 'draft';
+    const PENDING = 'pending';
+    const UNPUBLISHED = 'unpublished';
+
+    const FAVORITE = 'favorite';
+    const NONFAVORITE = 'usual';
 }
---
+++ database/migrations/2022_06_20_230022_add_columns_posts_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use App\Models\Post;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->increments('id')->change();
+            $table->unsignedInteger('parent_id')->nullable();
+            $table->string('title');
+            $table->string('meta_title')->nullable();
+            $table->string('slug')->nullable();
+            $table->text('summary')->nullable();
+            $table->char('published', 100)->default(Post::UNPUBLISHED);
+            $table->dateTime('published_at')->nullable();
+            $table->text('content')->nullable();
+            $table->unsignedInteger('views')->nullable();
+            $table->string('hero_image')->nullable();
+            $table->string('images')->nullable();
+            $table->unsignedSmallInteger('time_to_read')->nullable();
+            $table->char('favorite', 100)->default(Post::NONFAVORITE);
+
+            $table->foreignId('author_id')->constrained('users')->cascadeOnUpdate()->restrictOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            //
+        });
+    }
+};
