70fde4ea2d02b8573ada637141ea9729d3e8bc7a
grs2015 <85684748+grs2015@users.noreply.github.com>
2022-06-21 01:28:23 +0200

    Func: added columns in comments table, Comment model edited

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+
+    const PUBLISHED = 'published';
+    const PENDING = 'pending';
+    const UNPUBLISHED = 'unpublished';
 }
---
+++ database/migrations/2022_06_20_231836_add_columns_comments_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Models\Comment;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            $table->increments('id')->change();
+            $table->string('title');
+            $table->text('content')->nullable();
+            $table->dateTime('published_at')->nullable();
+            $table->char('published', 100)->default(Comment::UNPUBLISHED);
+
+            $table->foreignId('post_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            //
+        });
+    }
+};
