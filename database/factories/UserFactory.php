ed274f32c610aaab0ddf2872915386e79353a6dd
grs2015 <85684748+grs2015@users.noreply.github.com>
2022-06-21 12:12:26 +0200

    Func: UserFactory added

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,8 +2,9 @@
 
 namespace Database\Factories;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
 use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -18,9 +19,11 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name(),
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
+            'mobile' => $this->faker->phoneNumber(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ];
@@ -36,6 +39,45 @@ class UserFactory extends Factory
         return $this->state(function (array $attributes) {
             return [
                 'email_verified_at' => null,
+            ];
+        });
+    }
+
+    /**
+     * The User is admin
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    public function admin()
+    {
+        return $this->state(function () {
+            return [
+                'role' => User::ADMIN_USER
+            ];
+        });
+    }
+
+    /**
+     * The User is author
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    public function author()
+    {
+        return $this->state(function () {
+            return [
+                'role' => User::AUTHOR_USER
+            ];
+        });
+    }
+
+    /**
+     * The User is regular
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    public function regular()
+    {
+        return $this->state(function () {
+            return [
+                'role' => User::REGULAR_USER
             ];
         });
     }
