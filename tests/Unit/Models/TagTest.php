de786f07f419f16786f247d045bfc2ab39f83e96
grs2015 <85684748+grs2015@users.noreply.github.com>
2022-06-21 14:13:09 +0200

    Func: Unit-test for Tag model added

---
+++ tests/Unit/Models/TagTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Models\Tag;
+
+uses()->group('TagModel');
+
+it('test slug attribute when the tag is created', function() {
+    $tag = Tag::factory()->create(['title' => 'New Tag Entry']);
+
+    expect($tag->slug)->toEqual('new-tag-entry');
+});
