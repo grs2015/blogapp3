dd162f7fe51d323ff730154c6528b4d77e4d500a
grs2015 <85684748+grs2015@users.noreply.github.com>
2022-06-21 12:04:25 +0200

    Func: PostFactory added

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +19,68 @@ class PostFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'parent_id' => 1,
+            'author_id' => User::factory(),
+            'title' => $this->faker->sentence(3),
+            'meta_title' => $this->faker->sentence(),
+            'summary' => $this->faker->text(),
+            'content' => $this->faker->text(),
+            'views' => $this->faker->numberBetween(0, 50),
+            'hero_image' => $this->faker->imageUrl(640, 480, 'blog-image', true),
+            'images' => array($this->faker->imageUrl(), $this->faker->imageUrl()),
+            'time_to_read' => $this->faker->numberBetween(1, 20),
         ];
+    }
+
+    /**
+     * The Post is published
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    public function published()
+    {
+        return $this->state(function () {
+            return [
+                'published' => Post::PUBLISHED
+            ];
+        });
+    }
+
+    /**
+     * The Post is draft
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    public function draft()
+    {
+        return $this->state(function () {
+            return [
+                'published' => Post::DRAFT
+            ];
+        });
+    }
+
+    /**
+     * The Post is pending
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    public function pending()
+    {
+        return $this->state(function () {
+            return [
+                'published' => Post::PENDING
+            ];
+        });
+    }
+
+    /**
+     * The Post is unpublished
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    public function unpublished()
+    {
+        return $this->state(function () {
+            return [
+                'published' => Post::UNPUBLISHED
+            ];
+        });
     }
 }
