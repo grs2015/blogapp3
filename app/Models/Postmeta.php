d61fb9bee59a234b911eb4fc754fef871cc27cc4
grs2015 <85684748+grs2015@users.noreply.github.com>
2022-06-21 14:23:50 +0200

    Func: Postmeta model data added

--- app/Models/Postmeta.php
+++ app/Models/Postmeta.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 /**
  * App\Models\Postmeta
@@ -29,4 +30,11 @@ use Illuminate\Database\Eloquent\Model;
 class Postmeta extends Model
 {
     use HasFactory;
+
+    public $guarded = [];
+
+    public function post():BelongsTo
+    {
+        return $this->belongsTo(Post::class);
+    }
 }
