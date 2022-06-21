804e5c5ca5f7da67935a858413a795e5524919de
grs2015 <85684748+grs2015@users.noreply.github.com>
2022-06-21 02:47:02 +0200

    Func: added columns in baseinfos table

---
+++ database/migrations/2022_06_21_003230_add_columns_baseinfos_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('baseinfos', function (Blueprint $table) {
+            $table->increments('id')->change();
+            $table->string('title')->nullable();
+            $table->text('content')->nullable();
+            $table->string('meta_title')->nullable();
+            $table->string('hero_image')->nullable();
+            $table->string('address')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('email')->nullable();
+            $table->string('website')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('baseinfos', function (Blueprint $table) {
+            //
+        });
+    }
+};
