077f72a200a742c042b9a88938082dd3b59dd7bb
grs2015 <85684748+grs2015@users.noreply.github.com>
2022-06-21 15:13:01 +0200

    Func: Category model data added

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,8 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 /**
  * App\Models\Category
@@ -33,4 +36,38 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    public $guarded = [];
+
+    /**
+     * Get the route key for the model.
+     *
+     * @return string
+     */
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    /**
+     * Slug attributes creation while creating instance
+     * @return void
+     */
+    protected static function booted()
+    {
+        static::creating(function(Category $category) {
+            if (!$category->slug) {
+                $category->slug = Str::slug($category->title, '-');
+            }
+        });
+    }
+
+    /**
+     * Relationship to Post model
+     * @return BelongsToMany
+     */
+    public function posts():BelongsToMany
+    {
+        return $this->belongsToMany(Post::class);
+    }
 }
