ac657bcb43fd5bd2e43b9849a397bb9edcd5a01f
grs2015 <85684748+grs2015@users.noreply.github.com>
2022-06-22 11:13:29 +0200

    Func: routes for UserPostController added

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::name('users.posts.')->group(function() {
+    Route::get('/users/{user}/posts', [UserPostController::class, 'index'])->name('index');
+    Route::get('/users/{user}/posts/{post:slug}', [UserPostController::class, 'show'])->name('show');
+    Route::get('/users/{user}/posts/create', [UserPostController::class, 'create'])->name('create');
+    Route::get('/users/{user}/posts/{post:slug}/edit', [UserPostController::class, 'edit'])->name('edit');
+    Route::post('/users/{user}/posts', [UserPostController::class, 'store'])->name('store');
+    Route::put('/users/{user}/posts/{post:slug}', [UserPostController::class, 'update'])->name('update');
+    Route::delete('/users/{user}/posts/{post:slug}', [UserPostController::class, 'destroy'])->name('delete');
 });
