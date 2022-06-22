e964a8b98a3747eb1a4e57cb3f32ee12fa84c785
grs2015 <85684748+grs2015@users.noreply.github.com>
2022-06-22 11:36:09 +0200

    Func: PostRepositoryInterface added

---
+++ app/Interfaces/PostRepositoryInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Interfaces;
+
+use Illuminate\Support\Collection;
+use Illuminate\Database\Eloquent\Model;
+
+interface PostRepositoryInterface
+{
+    public function getAllEntries(int $userId):?Collection;
+    public function getEntryById(int $userId, int $postId):?Model;
+    public function deleteEntry(int $userId, int $postId):void;
+    public function createEntry(int $userId, array $postAttributes):?Model;
+    public function updateEntry(int $userId, array $newAttributes):?Model;
+}
