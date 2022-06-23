dd6038e8d3a741cd19402ff431682226b63ef970
grs2015 <85684748+grs2015@users.noreply.github.com>
2022-06-23 10:30:33 +0200

    Func: @index data added

--- app/Http/Controllers/UserPostController.php
+++ app/Http/Controllers/UserPostController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Interfaces\PostRepositoryInterface;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserPostController extends Controller
 {
+    private PostRepositoryInterface $postRepository;
+
+    public function __construct(PostRepositoryInterface $postRepository)
+    {
+        $this->postRepository = $postRepository;
+    }
     /**
      * Display a listing of the resource.
      *
@@ -15,7 +22,9 @@ class UserPostController extends Controller
      */
     public function index(User $user)
     {
-        //
+        $posts = $this->postRepository->getAllEntries($user->id);
+
+        return view('post.index', ['posts' => $posts ]);
     }
 
     /**
@@ -23,9 +32,9 @@ class UserPostController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create(User $user)
+    public function create()
     {
-        //
+        return view('post.create');
     }
 
     /**
@@ -36,7 +45,20 @@ class UserPostController extends Controller
      */
     public function store(Request $request, User $user)
     {
-        //
+        $validated = $request->validated();
+        // TODO - Form request for validation
+        // Form request can accept 'published' field only as 'unpublished'
+        // Or make it as an exception if received any other than 'unpublished'
+        // TODO - Allow storing only for Author users - later test this feature as well
+        // TODO - Store property view as zero
+        // TODO - Store functionality for hero-image
+        // TODO - Store functionality for post-images
+        // TODO - Request must contain category array, not null
+        // TODO - Request may contain tags array
+        // TODO - If category/tags IDs received with request, connect them as sync (M-2-M) to created post
+        // TODO - after successful creation send the notification to admin user
+        // TODO - Stored post must have statuses: unpublished and favotire: usual with views: zero
+        // TODO - Add library for image processing
     }
 
     /**
@@ -47,7 +69,8 @@ class UserPostController extends Controller
      */
     public function show(User $user, Post $post)
     {
-        //
+        // TODO - View increment functionality
+        // TODO - Corresponding View class
     }
 
     /**
@@ -58,7 +81,7 @@ class UserPostController extends Controller
      */
     public function edit(User $user, Post $post)
     {
-        //
+        // TODO - Corresponding View class
     }
 
     /**
@@ -70,7 +93,7 @@ class UserPostController extends Controller
      */
     public function update(Request $request, User $user, Post $post)
     {
-        //
+        // TODO - in test add the check of previous file deletion after update
     }
 
     /**
