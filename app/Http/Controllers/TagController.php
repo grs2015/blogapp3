965e1177d4c848b52261d61b8d7eaaddc5c54ce3
grs2015 <85684748+grs2015@users.noreply.github.com>
2022-06-28 11:03:14 +0200

    Func: basic implementation of TagController added

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -2,9 +2,48 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tag;
 use Illuminate\Http\Request;
 
 class TagController extends Controller
 {
-    //
+    public function __construct()
+    {
+
+    }
+
+    public function index()
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function store(Request $request)
+    {
+
+    }
+
+    public function show(Tag $tag)
+    {
+
+    }
+
+    public function edit(Tag $tag)
+    {
+
+    }
+
+    public function update(Request $reques, Tag $tag)
+    {
+
+    }
+
+    public function destroy(Tag $tag)
+    {
+
+    }
 }
