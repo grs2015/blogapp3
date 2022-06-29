5c3fae6ca0aaf29ff63407f4143d10430dba1148
grs2015 <85684748+grs2015@users.noreply.github.com>
2022-06-29 14:25:17 +0200

    Func: @update - tests added

--- tests/Feature/Http/Controllers/CategoryControllerTest.php
+++ tests/Feature/Http/Controllers/CategoryControllerTest.php
@@ -3,8 +3,11 @@
 use App\Models\User;
 use App\Models\Category;
 use App\Events\CategoryCreated;
+use App\Events\CategoryUpdated;
 use App\Http\Controllers\CategoryController;
 use App\Mail\CategoryCreatedNotificationMarkdown;
+use App\Mail\CategoryUpdatedNotificationMarkdown;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 
 uses()->group('Cat');
 
@@ -40,7 +43,7 @@ it('checks the validation and redirect', function() {
     $response->assertRedirect(route('categories.index'));
 });
 
-it('checks the incorrect parent_id validation', function() {
+it('checks the incorrect parent_id validation at creation', function() {
     $cat = Category::factory()->create(['id' => 1]);
     $catData = [
         'title' => 'New Cat',
@@ -136,4 +139,116 @@ it('renders edit form for cat by given slug', function() {
     $response->assertSee($cat->content);
     $response->assertSee($cat->slug);
     $response->assertDontSee($cat->summary);
+});
+/* ----------------------------- @update method ----------------------------- */
+it('checks the validation and redirect at update', function() {
+    $cat = Category::factory()->create();
+
+    $catData = [
+        'title' => 'Updated category',
+        'content' => 'Content of updated category'
+    ];
+
+    $response = $this->put(action([CategoryController::class, 'update'], ['category' => $cat->slug]), $catData);
+    $cat->refresh();
+
+    $response->assertSessionHasNoErrors();
+    $response->assertStatus(302);
+    $response->assertRedirect(action([CategoryController::class, 'edit'], ['category' => $cat->slug]));
+});
+
+it('checks the incorrect parent_id validation at update', function() {
+    $cat = Category::factory()->count(2)->state(new Sequence(['id' => 1], ['id' => 3]))->create();
+
+    $catData = [
+        'title' => 'Updated Category',
+        'parent_id' => 5
+    ];
+
+    $response = $this->put(action([CategoryController::class, 'update'], ['category' => $cat->find(3)->slug]), $catData);
+
+    $response->assertSessionHasErrors();
+});
+
+it('checks the updated cat is in database', function() {
+    $cat = Category::factory()->create();
+
+    $catData = [
+        'title' => 'Updated category',
+        'content' => 'Content of updated category'
+    ];
+
+    $response = $this->put(action([CategoryController::class, 'update'], ['category' => $cat->slug]), $catData);
+    $cat->refresh();
+
+    $this->assertDatabaseHas('categories', [
+        'title' => 'Updated category',
+    ]);
+    $response->assertStatus(302);
+    $response->assertRedirect(action([CategoryController::class, 'edit'], ['category' => $cat->slug]));
+});
+
+it('checks if the cat slug attribute updated according updated title attribute', function() {
+    $cat = Category::factory()->create(['title' => 'New Cat']);
+    expect($cat->slug)->toBe('new-cat');
+    $this->assertDatabaseHas('categories', ['slug' => $cat->slug]);
+
+    $catData = [
+        'title' => 'Updated Cat',
+    ];
+
+    $this->put(action([CategoryController::class, 'update'], ['category' => $cat->slug]), $catData);
+
+    $cat->refresh();
+    expect($cat->slug)->toBe('updated-cat');
+    $this->assertDatabaseHas('categories', ['slug' => $cat->slug]);
+});
+
+it('test the content of the CategoryUpdatedNotificationMarkdown mailable', function() {
+    $cat = Category::factory()->create();
+
+    $mailable = new CategoryUpdatedNotificationMarkdown('Category Title', 'Category Content');
+
+    $mailable->assertSeeInHtml(config('contacts.admin_email'));
+    $mailable->assertSeeInHtml('Category Title');
+    $mailable->assertSeeInHtml('Category Content');
+});
+
+it('checks the event firing after updating the cat in database', function() {
+    $cat = Category::factory()->create();
+    $catData = [
+        'title' => 'Newest cat',
+        'content' => 'Content of the newest cat',
+    ];
+    Event::fake();
+
+    $response = $this->put(action([CategoryController::class, 'update'], ['category' => $cat->slug]), $catData);
+
+    $response->assertStatus(302);
+    $response->assertSessionHasNoErrors();
+    Event::assertDispatched(CategoryUpdated::class);
+});
+
+it('checks the mails been queued from admin after updating the cat in database', function() {
+    Mail::fake();
+    $user = User::factory()->author()->create();
+    $cat = Category::factory()->create();
+    $catData = [
+        'title' => 'Newest cat',
+        'content' => 'Content of the newest cat',
+    ];
+
+    $response = $this->put(action([CategoryController::class, 'update'], ['category' => $cat->slug]), $catData);
+
+    $response->assertStatus(302);
+    $response->assertSessionHasNoErrors();
+    Mail::assertQueued(function(CategoryUpdatedNotificationMarkdown $mail) use ($catData, $user) {
+        if ( ! $mail->hasTo($user->email)) {
+            return false;
+        }
+        if ( $mail->title !== $catData['title'] ) {
+            return false;
+        }
+        return true;
+    });
 });
