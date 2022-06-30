de647ea34c1ded4c2212c41b6350ab0fbd023b34
grs2015 <85684748+grs2015@users.noreply.github.com>
2022-06-30 15:01:16 +0200

    Func: @index - method/view data added

--- app/Http/Controllers/BaseinfoController.php
+++ app/Http/Controllers/BaseinfoController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Interfaces\BaseinfoRepositoryInterface;
 
 class BaseinfoController extends Controller
 {
-    //
+    public function __construct(
+        private BaseinfoRepositoryInterface $baseinfoRepository
+    ) {  }
+
+    public function index()
+    {
+        $infos = $this->baseinfoRepository->getAllEntries();
+
+        return view('baseinfo.index', compact(['infos']));
+    }
 }
