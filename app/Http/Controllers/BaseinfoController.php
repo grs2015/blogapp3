99bf085cdf41135fc1b7db638b3dea5f9b288004
grs2015 <85684748+grs2015@users.noreply.github.com>
2022-06-30 17:00:15 +0200

    Func: @store - method/request data added

--- app/Http/Controllers/BaseinfoController.php
+++ app/Http/Controllers/BaseinfoController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use App\Http\Requests\StoreBaseinfoRequest;
 use App\Interfaces\BaseinfoRepositoryInterface;
 
 class BaseinfoController extends Controller
@@ -21,5 +23,23 @@ class BaseinfoController extends Controller
     public function create()
     {
         return view('baseinfo.create');
+    }
+
+    public function store(StoreBaseinfoRequest $request)
+    {
+        $validated = $request->safe()->except(['hero_image']);
+
+        if ($request->has('hero_image')) {
+            $file = $request->file('hero_image');
+            $timestamp = now()->format('Y-m-d-H-i-s');
+            $filename = "{$timestamp}-{$file->getClientOriginalName()}";
+
+            $path = Storage::putFileAs('uploads', $file, $filename);
+            $validated['hero_image'] = $path;
+        }
+
+        $this->baseinfoRepository->createEntry($validated);
+
+        return redirect()->action([BaseinfoController::class, 'index']);
     }
 }
