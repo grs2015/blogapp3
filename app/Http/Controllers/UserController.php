a41242259fecc31b40d6614122e8edcd67cca0d5
grs2015 <85684748+grs2015@users.noreply.github.com>
2022-06-30 22:46:18 +0200

    Func: @index - method/view data added

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Interfaces\UserRepositoryInterface;
 
 class UserController extends Controller
 {
-    //
+    public function __construct(
+        private UserRepositoryInterface $userRepository
+    ) {}
+
+    public function index()
+    {
+        $users = $this->userRepository->getAllEntries();
+
+        return view('user.index', compact(['users']));
+    }
+
+    public function show(User $user)
+    {
+
+    }
+
+    public function destroy(User $user)
+    {
+
+    }
 }
