23af50703783e5a4e029180afc9e59b430368166
grs2015 <85684748+grs2015@users.noreply.github.com>
2022-07-07 14:14:23 +0200

    Func: PostController@show - post view counter tests added

--- tests/Feature/Http/Controllers/PostControllerTest.php
+++ tests/Feature/Http/Controllers/PostControllerTest.php
@@ -62,3 +62,30 @@ it('renders the public post page with post data by slug', function() {
     $response->assertSee($postCategoryTitle);
     $response->assertSee($postPostmetaKey);
 });
+
+it('increases the post view count for new visitor', function() {
+    $post = Post::factory()
+        ->hasCategories(3)
+        ->create();
+    // Action #1
+    $response = $this->get(action([PostController::class, 'show'], ['post' => $post->slug]));
+    // Asserion #1 - views counter increased by 1
+    $response->assertOk();
+    $post->refresh();
+    $response->assertSeeText("Post views: {$post->views}");
+    expect($post->views)->toEqual(1);
+    // Action #2
+    $response = $this->get(action([PostController::class, 'show'], ['post' => $post->slug]));
+    // Assertion #2 - views counter doesn't increase - the session is not invalidated
+    $post->refresh();
+    $response->assertSessionHas('viewed_posts', array($post->id));
+    expect($post->views)->toEqual(1);
+
+    // Arrange #3 - session invalidation
+    session()->invalidate();
+    // Action #3
+    $response = $this->get(action([PostController::class, 'show'], ['post' => $post->slug]));
+    // Assertion #3 - views counter increases once again, after session invalidation
+    $post->refresh();
+    expect($post->views)->toEqual(2);
+});
