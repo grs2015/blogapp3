dd94d49c1d4e97a11cbb49e077ebcd5f30428eec
grs2015 <85684748+grs2015@users.noreply.github.com>
2022-07-08 15:03:47 +0200

    Func: Spatie MW added, routes tweaked

--- routes/web.php
+++ routes/web.php
@@ -41,62 +41,148 @@ Route::get('/', function () {
 Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 Route::get('/posts/{post:slug}', [PostController::class, 'show'])->name('posts.show');
 
-Route::post('posts/{post:slug}/rate', [PostRatingController::class, 'store'])->name('posts.rate')->middleware('auth');
-
-
-
 /* ------------------------------- Admin part ------------------------------- */
 // Dashboard
+// Here goes the root with Auth middleware
 
-// Specific user posts end-points
-Route::name('users.posts.')->group(function() {
-    Route::get('/users/{user}/posts', [UserPostController::class, 'index'])->name('index');
-    Route::get('/users/{user}/posts/create', [UserPostController::class, 'create'])->name('create');
-    Route::get('/users/{user}/posts/{post:slug}', [UserPostController::class, 'show'])->name('show');
-    Route::get('/users/{user}/posts/{post:slug}/edit', [UserPostController::class, 'edit'])->name('edit');
-    Route::post('/users/{user}/posts', [UserPostController::class, 'store'])->name('store');
-    Route::put('/users/{user}/posts/{post:slug}', [UserPostController::class, 'update'])->name('update');
-    Route::delete('/users/{user}/posts/{post:slug}', [UserPostController::class, 'destroy'])->name('delete');
+// Admin part
+Route::middleware('auth')->group(function() {
+    Route::prefix('admin')->group(function() {
+        Route::middleware('auth')->group(function() {
+            Route::name('admin.')->group(function() {
+                // Admin routes
+                Route::name('tags.')->group(function() {
+                    Route::get('/tags', [TagController::class, 'index'])->name('index');
+                    Route::get('/tags/create', [TagController::class, 'create'])->name('create');
+                    Route::get('/tags/{tag:slug}', [TagController::class, 'show'])->name('show');
+                    Route::get('/tags/{tag:slug}/edit', [TagController::class, 'edit'])->name('edit');
+                    Route::post('/tags', [TagController::class, 'store'])->name('store');
+                    Route::put('/tags/{tag:slug}', [TagController::class, 'update'])->name('update');
+                    Route::delete('/tags/{tag:slug}', [TagController::class, 'destroy'])->name('delete');
+                });
 
-    Route::post('/users/{user}/posts/delete', [UserPostTrashController::class, 'destroy'])->name('forcedelete');
-    Route::post('/users/{user}/posts/restore', [UserPostTrashController::class, 'restore'])->name('restore');
+                Route::name('categories.')->group(function() {
+                    Route::get('/categories', [CategoryController::class, 'index'])->name('index');
+                    Route::get('/categories/create', [CategoryController::class, 'create'])->name('create');
+                    Route::get('/categories/{category:slug}', [CategoryController::class, 'show'])->name('show');
+                    Route::get('/categories/{category:slug}/edit', [CategoryController::class, 'edit'])->name('edit');
+                    Route::post('/categories', [CategoryController::class, 'store'])->name('store');
+                    Route::put('/categories/{category:slug}', [CategoryController::class, 'update'])->name('update');
+                    Route::delete('/categories/{category:slug}', [CategoryController::class, 'destroy'])->name('delete');
+                });
+
+                Route::resource('posts.postmetas', PostPostmetaController::class);
+                Route::resource('/baseinfo', BaseinfoController::class);
+                Route::resource('posts.comments', PostCommentController::class);
+
+                Route::name('users.')->group(function() {
+                    Route::get('/users', [UserController::class, 'index'])->name('index');
+                    Route::get('/users/{user}', [UserController::class, 'show'])->name('show');
+                    Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('destroy');
+
+                    Route::post('/users/delete', [UserTrashController::class, 'destroy'])->name('forcedelete');
+                    Route::post('/users/restore', [UserTrashController::class, 'restore'])->name('restore');
+                });
+
+                Route::name('users.posts.')->group(function() {
+                    Route::get('/users/{user}/posts', [UserPostController::class, 'index'])->name('index');
+                    Route::get('/users/{user}/posts/create', [UserPostController::class, 'create'])->name('create');
+                    Route::get('/users/{user}/posts/{post:slug}', [UserPostController::class, 'show'])->name('show');
+                    Route::get('/users/{user}/posts/{post:slug}/edit', [UserPostController::class, 'edit'])->name('edit');
+                    Route::post('/users/{user}/posts', [UserPostController::class, 'store'])->name('store');
+                    Route::put('/users/{user}/posts/{post:slug}', [UserPostController::class, 'update'])->name('update');
+                    Route::delete('/users/{user}/posts/{post:slug}', [UserPostController::class, 'destroy'])->name('delete');
+
+                    Route::post('/users/{user}/posts/delete', [UserPostTrashController::class, 'destroy'])->name('forcedelete');
+                    Route::post('/users/{user}/posts/restore', [UserPostTrashController::class, 'restore'])->name('restore');
+                });
+            });
+        });
+    });
+
+    Route::prefix('author')->group(function() {
+        Route::middleware('role:author')->group(function() {
+            Route::name('user')->group(function() {
+                // Users routes
+                Route::name('posts.')->group(function() {
+                    Route::get('/users/{user}/posts', [UserPostController::class, 'index'])->name('index');
+                    Route::get('/users/{user}/posts/create', [UserPostController::class, 'create'])->name('create');
+                    Route::get('/users/{user}/posts/{post:slug}', [UserPostController::class, 'show'])->name('show');
+                    Route::get('/users/{user}/posts/{post:slug}/edit', [UserPostController::class, 'edit'])->name('edit');
+                    Route::post('/users/{user}/posts', [UserPostController::class, 'store'])->name('store');
+                    Route::put('/users/{user}/posts/{post:slug}', [UserPostController::class, 'update'])->name('update');
+                    Route::delete('/users/{user}/posts/{post:slug}', [UserPostController::class, 'destroy'])->name('delete');
+
+                    Route::post('/users/{user}/posts/delete', [UserPostTrashController::class, 'destroy'])->name('forcedelete');
+                    Route::post('/users/{user}/posts/restore', [UserPostTrashController::class, 'restore'])->name('restore');
+                });
+            });
+        });
+    });
+
+    Route::prefix('member')->group(function() {
+        Route::middleware('role:member')->group(function() {
+            Route::name('member')->group(function() {
+                // Member routes
+                Route::post('posts/{post:slug}/rate', [PostRatingController::class, 'store'])->name('rate');
+                Route::post('posts/{post:slug}/comment', [PostCommentController::class, 'store'])->name('comment');
+            });
+        });
+    });
+
 });
 
-Route::name('tags.')->group(function() {
-    Route::get('/tags', [TagController::class, 'index'])->name('index');
-    Route::get('/tags/create', [TagController::class, 'create'])->name('create');
-    Route::get('/tags/{tag:slug}', [TagController::class, 'show'])->name('show');
-    Route::get('/tags/{tag:slug}/edit', [TagController::class, 'edit'])->name('edit');
-    Route::post('/tags', [TagController::class, 'store'])->name('store');
-    Route::put('/tags/{tag:slug}', [TagController::class, 'update'])->name('update');
-    Route::delete('/tags/{tag:slug}', [TagController::class, 'destroy'])->name('delete');
-});
 
-Route::name('categories.')->group(function() {
-    Route::get('/categories', [CategoryController::class, 'index'])->name('index');
-    Route::get('/categories/create', [CategoryController::class, 'create'])->name('create');
-    Route::get('/categories/{category:slug}', [CategoryController::class, 'show'])->name('show');
-    Route::get('/categories/{category:slug}/edit', [CategoryController::class, 'edit'])->name('edit');
-    Route::post('/categories', [CategoryController::class, 'store'])->name('store');
-    Route::put('/categories/{category:slug}', [CategoryController::class, 'update'])->name('update');
-    Route::delete('/categories/{category:slug}', [CategoryController::class, 'destroy'])->name('delete');
-});
 
-Route::get('/comments/create', [PostCommentController::class, 'create'])->name('comments.create');
-Route::resource('posts.comments', PostCommentController::class);
+// // Specific user posts end-points
+// Route::name('users.posts.')->group(function() {
+//     Route::get('/users/{user}/posts', [UserPostController::class, 'index'])->name('index');
+//     Route::get('/users/{user}/posts/create', [UserPostController::class, 'create'])->name('create');
+//     Route::get('/users/{user}/posts/{post:slug}', [UserPostController::class, 'show'])->name('show');
+//     Route::get('/users/{user}/posts/{post:slug}/edit', [UserPostController::class, 'edit'])->name('edit');
+//     Route::post('/users/{user}/posts', [UserPostController::class, 'store'])->name('store');
+//     Route::put('/users/{user}/posts/{post:slug}', [UserPostController::class, 'update'])->name('update');
+//     Route::delete('/users/{user}/posts/{post:slug}', [UserPostController::class, 'destroy'])->name('delete');
 
-Route::get('/postmetas/create', [PostPostmetaController::class, 'create'])->name('postmetas.create');
-Route::resource('posts.postmetas', PostPostmetaController::class);
+//     Route::post('/users/{user}/posts/delete', [UserPostTrashController::class, 'destroy'])->name('forcedelete');
+//     Route::post('/users/{user}/posts/restore', [UserPostTrashController::class, 'restore'])->name('restore');
+// });
 
-Route::get('/baseinfo/create', [BaseinfoController::class, 'create'])->name('baseinfos.create');
-Route::resource('/baseinfo', BaseinfoController::class);
+// Route::name('tags.')->group(function() {
+//     Route::get('/tags', [TagController::class, 'index'])->name('index');
+//     Route::get('/tags/create', [TagController::class, 'create'])->name('create');
+//     Route::get('/tags/{tag:slug}', [TagController::class, 'show'])->name('show');
+//     Route::get('/tags/{tag:slug}/edit', [TagController::class, 'edit'])->name('edit');
+//     Route::post('/tags', [TagController::class, 'store'])->name('store');
+//     Route::put('/tags/{tag:slug}', [TagController::class, 'update'])->name('update');
+//     Route::delete('/tags/{tag:slug}', [TagController::class, 'destroy'])->name('delete');
+// });
+
+// Route::name('categories.')->group(function() {
+//     Route::get('/categories', [CategoryController::class, 'index'])->name('index');
+//     Route::get('/categories/create', [CategoryController::class, 'create'])->name('create');
+//     Route::get('/categories/{category:slug}', [CategoryController::class, 'show'])->name('show');
+//     Route::get('/categories/{category:slug}/edit', [CategoryController::class, 'edit'])->name('edit');
+//     Route::post('/categories', [CategoryController::class, 'store'])->name('store');
+//     Route::put('/categories/{category:slug}', [CategoryController::class, 'update'])->name('update');
+//     Route::delete('/categories/{category:slug}', [CategoryController::class, 'destroy'])->name('delete');
+// });
+
+// Route::get('/comments/create', [PostCommentController::class, 'create'])->name('comments.create');
+// Route::resource('posts.comments', PostCommentController::class);
+
+// Route::get('/postmetas/create', [PostPostmetaController::class, 'create'])->name('postmetas.create');
+// Route::resource('posts.postmetas', PostPostmetaController::class);
+
+// Route::get('/baseinfo/create', [BaseinfoController::class, 'create'])->name('baseinfos.create');
+// Route::resource('/baseinfo', BaseinfoController::class);
 
 // Other routes are assigned thru Fortify package
-Route::name('users.')->group(function() {
-    Route::get('/users', [UserController::class, 'index'])->name('index');
-    Route::get('/users/{user}', [UserController::class, 'show'])->name('show');
-    Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('destroy');
+// Route::name('users.')->group(function() {
+//     Route::get('/users', [UserController::class, 'index'])->name('index');
+//     Route::get('/users/{user}', [UserController::class, 'show'])->name('show');
+//     Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('destroy');
 
-    Route::post('/users/delete', [UserTrashController::class, 'destroy'])->name('forcedelete');
-    Route::post('/users/restore', [UserTrashController::class, 'restore'])->name('restore');
-});
+//     Route::post('/users/delete', [UserTrashController::class, 'destroy'])->name('forcedelete');
+//     Route::post('/users/restore', [UserTrashController::class, 'restore'])->name('restore');
+// });
