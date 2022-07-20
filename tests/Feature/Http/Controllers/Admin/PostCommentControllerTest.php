0e0cf3a4bd08543ad9169b1520f5ac1c0ba758c5
grs2015 <85684748+grs2015@users.noreply.github.com>
2022-07-20 22:31:09 +0200

    Func: FE compiled with Inertia/Vue, Inertia tests added, all tests tweaked

--- tests/Feature/Http/Controllers/Admin/PostCommentControllerTest.php
+++ tests/Feature/Http/Controllers/Admin/PostCommentControllerTest.php
@@ -1,44 +1,107 @@
 <?php
 
+use App\Enums\CommentStatus;
 use App\Models\Post;
 use App\Models\Comment;
 use App\Http\Controllers\Admin\PostCommentController;
+use Inertia\Testing\AssertableInertia as Assert;
 
 
-uses()->group('admin');
+uses()->group('admin', 'postcomment');
 
 beforeEach(function() {
     $this->seed(RolePermissionSeeder::class);
     loginAsAdmin();
 });
 /* ------------------------------ @index method ----------------------------- */
-it('renders the comment page with comments data', function() {
+it('renders the comments page with Inertia', function() {
     $post = Post::factory()->hasComments(3)->create();
-    $comments = $post->comments;
-    $commentTitle = $comments[random_int(0, 2)]->title;
+    $commentTitle = $post->comments[0]->title;
 
     $response = $this->get(action([PostCommentController::class, 'index'], ['post' => $post->slug]));
 
     $response->assertOk();
-    $response->assertSee('All comments attached to Post');
-    $response->assertSee($post->title);
-    $response->assertSee($commentTitle);
+    $response->assertInertia(fn (Assert $page) => $page
+        ->component('Comment/Index')
+        ->has('model', fn(Assert $page) => $page
+            ->has('comments', 3)
+            ->has('comments.0', fn(Assert $page) => $page
+                ->where('title', $commentTitle)
+                ->etc()
+            )
+        )
+    );
 });
 
 /* ----------------------------- @create method ----------------------------- */
-it('renders create comment form', function() {
+it('renders create comment form with Inertia', function() {
     $post = Post::factory()->create();
-    $this->get(action([PostCommentController::class, 'create'], ['post' => $post->slug]))->assertSee('Form for comment creation');
+    $response = $this->get(action([PostCommentController::class, 'create'], ['post' => $post->slug]));
+
+    $response->assertOk();
+    $response->assertInertia(fn (Assert $page) => $page
+        ->component('Comment/Form')
+        ->has('model', fn(Assert $page) => $page
+            ->has('comment')
+            ->missing('comment.address')
+            ->etc()
+            )
+        );
 });
 
+/* ------------------------------ @edit method ------------------------------ */
+it('renders edit form for single comment with Inertia', function() {
+    $post = Post::factory()->hasComments(3)->create();
+    $comment = Comment::first();
+    $commentTitle = $comment->title;
+
+    $response = $this->get(action([PostCommentController::class, 'edit'], ['post' => $post->slug, 'comment' => $comment->id]));
+
+    $response->assertOk();
+    $response->assertInertia(fn (Assert $page) => $page
+        ->component('Comment/Form')
+        ->has('model', fn(Assert $page) => $page
+            ->has('comment', fn(Assert $page) => $page
+                ->where('title', $commentTitle)
+                ->etc()
+            )
+        )
+    );
+});
+
+/* ------------------------------ @show method ------------------------------ */
+it('renders single comment entry with Inertia', function() {
+    $this->withoutExceptionHandling();
+
+    $post = Post::factory()->hasComments(3)->create();
+    $comment = Comment::first();
+    $commentTitle = $comment->title;
+
+    $response = $this->get(action([PostCommentController::class, 'show'], ['post' => $post->slug, 'comment' => $comment->id]));
+
+    $response->assertOk();
+    $response->assertInertia(fn (Assert $page) => $page
+        ->component('Comment/Show')
+        ->has('model', fn(Assert $page) => $page
+            ->has('comment', fn(Assert $page) => $page
+                ->where('title', $commentTitle)
+                ->etc()
+            )
+        )
+    );
+});
+
+
+
 /* ------------------------------ @store method ----------------------------- */
-it('checks the validation and redirect', function() {
+it('checks the comment data validation and redirect', function() {
     $this->withoutExceptionHandling();
     $post = Post::factory()->create();
     $commentData = [
         'title' => 'New Comment',
         'content' => 'Content of comment',
-        'published_at' => now()
+        'published_at' => now(),
+        'post_id' => $post->id
     ];
 
     $response = $this->post(action([PostCommentController::class, 'store'],['post' => $post->slug]), $commentData);
@@ -59,56 +122,33 @@ it('checks the session error when validation fails at storing', function() {
     $response->assertSessionHasErrors();
 });
 
-it('checks the stored comment has some predefined properties and resides in database', function() {
+it('checks the stored comment data has some predefined properties and resides in database', function() {
     $post = Post::factory()->create();
     $time = now();
     $commentData = [
         'title' => 'New Comment',
         'content' => 'Content of comment',
-        'published_at' => $time
+        'published_at' => $time,
+        'post_id' => $post->id
     ];
 
     $this->post(action([PostCommentController::class, 'store'],['post' => $post->slug]), $commentData);
 
     $this->assertDatabaseHas('comments', [
-        'published' => Comment::UNPUBLISHED,
+        'status' => 'pending',
         'title' => 'New Comment',
-        'published_at' => $time
     ]);
 });
 
-/* ------------------------------ @show method ------------------------------ */
-it('renders single comment entry by given ID', function() {
-    $post = Post::factory()->hasComments(3)->create();
-    $comment = Comment::first();
-
-    $response = $this->get(action([PostCommentController::class, 'show'], ['post' => $post->slug, 'comment' => $comment->id]));
-
-    $response->assertSee($comment->title);
-    $response->assertSee($post->title);
-    $response->assertDontSee($post->content);
-});
-
-/* ------------------------------ @edit method ------------------------------ */
-it('renders edit form for single comment entry by given ID', function() {
-    $post = Post::factory()->hasComments(3)->create();
-    $comment = Comment::first();
-
-    $response = $this->get(action([PostCommentController::class, 'edit'], ['post' => $post->slug, 'comment' => $comment->id]));
-
-    $response->assertSee($comment->title);
-    $response->assertSee($comment->published);
-    $response->assertDontSee($comment->summary);
-    $response->assertSee($post->title);
-});
-
 /* ----------------------------- @update method ----------------------------- */
-it('checks the validation and redirect at update', function() {
+it('checks the comment data for validation and redirect at update', function() {
     $post = Post::factory()->hasComments(3)->create();
     $commentData = [
         'title' => 'Updated comment',
         'content' => 'Updated comment content',
-        'published_at' => now()->addHours(5)
+        'published_at' => now()->addHours(5),
+        'post_id' => $post->id,
+        'id' => Comment::first()->id
     ];
 
     $response = $this->put(action([PostCommentController::class, 'update'], ['post' => $post->slug, 'comment' => Comment::first()->id]), $commentData);
@@ -123,7 +163,9 @@ it('checks the updated comment is in database', function() {
     $commentData = [
         'title' => 'Updated comment',
         'content' => 'Updated comment content',
-        'published_at' => now()->addHours(5)
+        'published_at' => now()->addHours(5),
+        'post_id' => $post->id,
+        'id' => Comment::first()->id
     ];
 
     $this->put(action([PostCommentController::class, 'update'], ['post' => $post->slug, 'comment' => Comment::first()->id]), $commentData);
@@ -151,7 +193,6 @@ it('checks the deletion of entry', function() {
     $response = $this->delete(action([PostCommentController::class, 'destroy'], ['post' => $post->slug, 'comment' => Comment::first()->id]));
 
     $response->assertRedirect(route('admin.posts.comments.index', ['post' => $post->slug]));
-    $this->assertSoftDeleted($comment);
-    // $this->assertModelMissing($comment);
-    // $this->assertDatabaseMissing('comments', $comment->toArray());
+    $this->assertModelMissing($comment);
+    $this->assertDatabaseMissing('comments', $comment->toArray());
 });
