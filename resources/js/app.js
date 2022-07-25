92d411bfb6f60d5c9ac9f5780f3b5286370c0be5
grs2015 <85684748+grs2015@users.noreply.github.com>
2022-07-25 16:46:12 +0200

    Setup: Quasar installed

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,9 @@ import { InertiaProgress } from '@inertiajs/progress'
 import { i18nVue } from 'laravel-vue-i18n'
 import Layout from '@/Shared/Layout.vue'
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { Quasar } from 'quasar'
+import '@quasar/extras/material-icons/material-icons.css'
+import 'quasar/src/css/index.sass'
 
 InertiaProgress.init()
 
@@ -20,6 +23,9 @@ createInertiaApp({
       .use(i18nVue, {
         resolve: (lang) => import(`../../lang/${lang}.json`)
         })
+      .use(Quasar, {
+        plugins: {}, // import Quasar plugins and add here
+      })
       .mount(el)
   },
 })
