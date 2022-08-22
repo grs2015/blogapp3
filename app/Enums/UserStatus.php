eb2d4ecd2b745de97865c46d5064ac45c3af54a9
grs2015 <85684748+grs2015@users.noreply.github.com>
2022-08-22 11:53:16 +0200

    Func: BE - UserStatus Enum updated, custom Exception added

--- app/Enums/UserStatus.php
+++ app/Enums/UserStatus.php
@@ -7,4 +7,15 @@ enum UserStatus: string
     case Enabled = 'enabled';
     case Disabled = 'disabled';
     case Pending = 'pending';
+
+    public function canBeDeleted(): bool
+    {
+        return match($this) {
+            self::Enabled => 'false',
+            self::Disabled => 'false',
+            self::Pending => 'true'
+        };
+    }
 }
+
+
